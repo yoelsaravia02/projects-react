f3972a7cb04bb2a25f3bf2a280cb85ac8185a0fa
Joel Saravia <yoel.saravia25@gmail.com>
2024-06-18 01:15:03 -0300

    wifi choto

--- form-dds/src/App.jsx
+++ form-dds/src/App.jsx
@@ -34,22 +34,37 @@ const App = () => {
 
       <label htmlFor="correo">Correo</label>
       <input type="email" id="correo" 
-      {...register("correo")}
+      {...register("correo", {
+        required: true,
+        pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/i
+      })}
       />
 
       <label htmlFor="contraseña">Contraseña</label>
       <input type="password" id="contraseña" 
-      {...register("contraseña")}
+      {...register("contraseña",
+        {
+          required: true
+        }
+      )}
       />
 
       <label htmlFor="confirmarPassword">Confirmar contraseña</label>
       <input type="password" id="confirmarPassword"
-      {...register("confirmarPassword")}
+      {...register("confirmarPassword", 
+        {
+          required: true
+        }
+      )}
       />
 
       <label htmlFor="fechaNacimiento">Fecha de nacimiento</label>
       <input type="date" id="fechaNacimiento"
-      {...register("fechaNacimiento")}
+      {...register("fechaNacimiento", 
+        {
+          required: true
+        }
+      )}
       />
 
       <label htmlFor="pais">Pais</label>
@@ -67,7 +82,11 @@ const App = () => {
       
       <label htmlFor="terminos">Acepto los terminos y condiciones</label>
       <input type="checkbox" id="terminos"
-      {...register("terminos")}
+      {...register("terminos",
+        {
+          required: true
+        }
+      )}
       />
 
       <button type="submit">Enviar</button>
